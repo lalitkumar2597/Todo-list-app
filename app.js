8fa4cef557ab319a3f3bfc8a682b35de7ca79374
lalit kumar <lalitkumarbhatiwal@gmail.com>
2022-07-08 16:26:18 +0530

    try update 2 after changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const app = express();
+const server = http.createServer(app);
 
 app.set('view engine', 'ejs');
 
@@ -125,6 +126,6 @@ app.get("/about", function(req, res){
 });
 
 
-app.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
